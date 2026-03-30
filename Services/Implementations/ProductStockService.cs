78dc3c351a622decfedf26ce70030580ae4ae600
LuigiTrue <luigipaoli100@gmail.com>
2026-03-30 09:31:51 -0300

    feat: Inicio do desenvolvimento dos serviços

--- Services/Implementations/ProductStockService.cs
+++ Services/Implementations/ProductStockService.cs
@@ -7,19 +7,8 @@ public class ProductStockService : IProductStockService
 {
     public Task<List<ProductStock>> GetProductStocksAsync()
     {
-        var random = new Random();
-
-        var stockList = new[]
-        {
-            "Farmácia Central",
-            "Farmácia Centro Cirúrgico",
-            "Fracionamento",
-            "Almoxarifado"
-        };
 
         var stocks = new List<ProductStock>();
-
-
 
         return Task.FromResult(stocks);
     }
